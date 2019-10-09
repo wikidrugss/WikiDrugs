6fea87b2e89194f564fd9209882e805e1c6b5b39
Vina del Mar <vina@duoc.acad>
2019-10-09 13:52:51 -0300

    post

--- mysite/drugs/views.py
+++ mysite/drugs/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post 
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'drugs/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'drugs/post_list.html', {'posts': posts })
 
